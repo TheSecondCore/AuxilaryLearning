16fbd1c96d635fab82fe411a55e41a7e6a706e4b
Martin Bagniuk <martin.bagniuk@gmail.com>
2019-07-10 00:24:42 +0300

    shortestPalindrome first itteration

---
+++ shortestPalindrome.js
@@ -0,0 +1,11 @@
+shortestPalindrome = str => {
+    let center = str.length-1
+    while (str[center-1] === str[center]) {center--}
+    for (let i = center - 1; i >= 0 ; i--) {
+        str = str+str[i]
+    }
+    return str
+}
+
+console.log(shortestPalindrome('race')) // One character in the center test
+console.log(shortestPalindrome('abcdeee')) // Multiple of the same char in the center test
