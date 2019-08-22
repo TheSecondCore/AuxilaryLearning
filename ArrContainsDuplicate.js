ac34d1599adcfa19c1ebc5c942866ba6c60746cf
Martin Bagniuk <martin.bagniuk@gmail.com>
2019-08-22 17:15:07 +0300

    Add a sorting solution for Contains Duplicates exercise

--- ArrContainsDuplicate.js
+++ ArrContainsDuplicate.js
@@ -9,6 +9,20 @@ const arrContainsDuplicate = arr => {
     return false
 }
 
-// O(n) but uses a lot of memory
+// O(n) but uses O(n) auxilary space
+
+console.log(arrContainsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+
+const arrContainsDuplicate = arr => {
+    arr.sort()
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i+1]) {
+            return true
+        }
+    }
+    return false
+}
+
+// O(n), uses O(1) auxilary space, but need to sort first
 
 console.log(arrContainsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
