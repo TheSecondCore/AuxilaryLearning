a18f175c772cea6af52b4d1d038788f0aca4590d
Martin Bagniuk <martin.bagniuk@gmail.com>
2019-08-27 22:49:15 +0300

    Solve minimum in rotated sorted array with random pivot

---
+++ minInRotatedSortedArray.js
@@ -0,0 +1,15 @@
+const findMin = arr => {
+    let left = 0, right = arr.length - 1
+    while (right - left > 1) {
+        let mid = left + ((right - left) >> 1)
+        if (arr[mid] > arr[right]) {
+            left = mid
+        } else {
+            right = mid
+        }
+    }
+    return Math.min(arr[left], arr[right])
+}
+
+console.log(findMin([3, 4, 5, 1, 2])) // 1
+console.log(findMin([4, 5, 6, 7, 0, 1, 2])) // 0
