d77ac56bdb8a900ca1f8b4dbf48fefdfd07e96d1
Bahniuk <martin.bahniuk@sigmaukraine.com>
2019-10-29 18:20:26 +0200

    Refactor mergeSort, Make merge into a Pure Function.

--- sorts/mergeSort.js
+++ sorts/mergeSort.js
@@ -1,32 +1,27 @@
 const merge = (left, right) => {
-  const resultArray = []
-  let leftIndex = 0, rightIndex = 0
+  const arr = []
 
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      resultArray.push(left[leftIndex])
-      leftIndex++
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      arr.push(left.shift())
     } else {
-      resultArray.push(right[rightIndex])
-      rightIndex++
+      arr.push(right.shift())
     }
   }
 
-  return resultArray
-    .concat(left.slice(leftIndex))
-    .concat(right.slice(rightIndex))
+  return [...arr, ...left, ...right]
 }
 
 const mergeSort = arr => {
   if (arr.length < 2) {
     return arr;
   }
-  const middle = Math.floor(arr.length / 2)
 
+  const middle = Math.floor(arr.length / 2)
   const left = arr.slice(0, middle)
   const right = arr.slice(middle)
 
   return merge(mergeSort(left), mergeSort(right))
 }
 
-console.log(mergeSort([10,5,3,8,2,2,6,4,18,4,2,-4,-23,3])) // [-23, -4, 2, 2, 2, 3, 3, 4, 4, 5, 6, 8, 10, 18]
+console.log(mergeSort([10, 5, 3, 8, 2, 2, 6, 4, 18, 4, 2, -4, -23, 3])) // [-23, -4, 2, 2, 2, 3, 3, 4, 4, 5, 6, 8, 10, 18]
