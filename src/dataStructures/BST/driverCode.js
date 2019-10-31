50306e8d83f83538c3b92ce1c6a03c8ca0d313e5
Martin Bagniuk <martin.bahniuk@sigmaukraine.com>
2019-10-31 15:31:20 +0200

    Finish the driver code for the BST.

--- src/dataStructures/BST/driverCode.js
+++ src/dataStructures/BST/driverCode.js
@@ -8,3 +8,10 @@ bst.insertNode(6);
 bst.insertNode(12);
 bst.insertNode(64);
 bst.insertNode(-123);
+bst.insertNode(1);
+bst.insertNode(12);
+bst.insertNode(23);
+bst.insertNode(60);
+bst.insertNode(14);
+
+console.log(bst.findNode(12));
